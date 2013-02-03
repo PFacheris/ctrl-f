4b320c8af0e3464e54714f61f92479460a080693
Chae Jubb <cjubb39@gmail.com>
2013-02-02 21:12:10 -0500

    testing parsing support of createUser method

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ var app = express();
 var mongojs = require('mongojs');
 var mongoUri = process.env.MONGOLAB_URI || 
   process.env.MONGOHQ_URL || 
-  'mongodb://localhost/mydb';
+  'mongodb://ctrl-f.herokuapps.com/mydb';
 var collections = ["users", "packages"];
 var db = mongojs(mongoUri, collections);
+
+var url = require('url');
 
 /*
  * Listening Port
@@ -35,9 +37,28 @@ app.post('/register', function(request, response) {
 
 // MongoDB 
 app.get('/createUser', function(request, response){
+  var name, age
+  var queryData = url.parse(request.url, true).query;
+  
+  if (queryData.name) {
+    name = queryData.name;
+  } else {
+    name = null;
+  }
+
+  if (queryData.age) {
+    age = queryData.name;
+  } else {
+    age = null;
+  }
+
   var newPerson = {
-        name: "Chae Jubb",
-        age: "18"
+        name: name,
+        age: age
   };
   db.users.save(newPerson);
+});
+
+app.get('/listUsers', function(request, response){
+  db.users.find()
 });
