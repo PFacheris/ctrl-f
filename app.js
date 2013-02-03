d55a2fa93a9c00b4b2316d9a69d0a66be44e9e84
pfacheris <plfacheris@gmail.com>
2013-02-03 18:01:48 -0500

    test 7

--- app.js
+++ app.js
@@ -1,69 +1,77 @@
 /*
  * Requires and initialization.
  */
-
 //Dependencies
 var express = require("express"),
     path = require("path");
 
-var MONGO = require('mongodb').MongoClient,
+var MONGO = require('mongodb'),
+    MONGO_CLIENT = MONGO.MongoClient,
     MONGO_URI = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb',
-    BSON = MONGO.BSONPure,
+    BSON = MONGO.pure().BSON,
     db = null;
 
-MONGO.connect(MONGO_URI, function (err, db) {
+MONGO_CLIENT.connect(MONGO_URI, function (err, db) {
+    if (err)
+    {
+        console.log('No DB found. Closing.");
+        return;
+    }
 
-console.log(db);
+    var auth = require('./auth')(db, BSON),
+        user = require('./routes/users')(db, BSON);
 
-var auth = require('./auth')(db, BSON),
-    user = require('./routes/users')(db, BSON);
+    var app = express();
 
-var app = express();
+    var application_root = process.cwd();
 
-var application_root = process.cwd();
+    /*
+     * Listening Port
+     */
+    var port = process.env.PORT || 5000;
+    app.listen(port, function () {
+        console.log("Listening on " + port);
+    });
 
-/*
- * Listening Port
- */
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+    /*
+     * Application Logic
+     */
 
-/*
- * Application Logic
- */
+    // Session Storage
+    var sessionStore = new express.session.MemoryStore;
 
-// Session Storage
-var sessionStore = new express.session.MemoryStore;
- 
-// Config
-app.configure(function () {
-  app.set('title', 'ctrl-f API');
-  app.use(express.cookieParser("secret"))
-  app.use(express.session({store: sessionStore})); 
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(application_root, "public")));
-  app.use('/images', express.static(path.join(application_root, "public", "images")));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+    // Config
+    app.configure(function () {
+        app.set('title', 'ctrl-f API');
+        app.use(express.cookieParser("secret"))
+        app.use(express.session({
+            store: sessionStore
+        }));
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(app.router);
+        app.use(express.static(path.join(application_root, "public")));
+        app.use('/images', express.static(path.join(application_root, "public", "images")));
+        app.use(express.errorHandler({
+            dumpExceptions: true,
+            showStack: true
+        }));
+    });
 
-app.get('/', function(request, response) {
-    response.redirect("/index.html");
-});
- 
-// Authentication
-app.post('/api:key', auth.setKey);
-app.get('/api/new', auth.newKey);
-app.get('/api', auth.isAuth);
+    app.get('/', function (request, response) {
+        response.redirect("/index.html");
+    });
 
-// User Actions
-app.post('/user/new', auth.isAuth, user.create);
-app.put('/user:id', auth.isAuth, user.update);
-app.get('/users', user.getAll);
-app.get('/user:id', auth.isAuth, user.getByID);
-app.get('/user:email', auth.isAuth, user.getByEmail);
-app.delete('/user:id', auth.isAuth, user.destroy);
+    // Authentication
+    app.post('/api:key', auth.setKey);
+    app.get('/api/new', auth.newKey);
+    app.get('/api', auth.isAuth);
+
+    // User Actions
+    app.post('/user/new', auth.isAuth, user.create);
+    app.put('/user:id', auth.isAuth, user.update);
+    app.get('/users', user.getAll);
+    app.get('/user:id', auth.isAuth, user.getByID);
+    app.get('/user:email', auth.isAuth, user.getByEmail);
+    app.delete('/user:id', auth.isAuth, user.destroy);
 });
