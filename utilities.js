8f30c6e1eb2ec63ca303b4c096707119e4a8ccf9
Chae Jubb <cjubb39@gmail.com>
2013-02-05 18:44:30 -0500

    rewrite hash function

--- utilities.js
+++ utilities.js
@@ -1,6 +1,6 @@
 var COLLECTION_NAME = 'users';
 exports.pwHash = function(password) {
-    var hash = 0;
+    var hash, hash1 = 0, hash2 = 0;
     var code;
             
             /*for (var i = 0; i < password.length; i++) {
@@ -11,9 +11,25 @@ exports.pwHash = function(password) {
                 }
             }*/
 
+    var odd = true;
     for (var i = 0; i < password.length; i++) {
         code = password.charCodeAt(i);
-        hash += code;
+        
+        if (odd) {
+            for (var j = 0; j < i; j++) {
+                hash1 *= code;
+            }
+            odd = false;
+        } else {
+            for (var j = 0; j < i; j++) {
+                hash2 += code;
+            }
+            odd = true;
+        }
+
+      
     }
+    hash = hash1 ^ hash2;
+
     return hash;
 }
