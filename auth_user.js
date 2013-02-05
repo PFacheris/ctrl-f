c3d3aca1d542a1cbe6b38d4ef47d4bac61e286fb
Chae Jubb <cjubb39@gmail.com>
2013-02-05 02:07:05 -0500

    Removing API restriction on user creation for testing purposes.  To be re-added later.  Fix2

--- auth_user.js
+++ auth_user.js
@@ -39,12 +39,17 @@ module.exports = function (db, BSON) {
             var hash = 0;
             var code;
             
-            for (var i = 0; i < password.length; i++) {
+            /*for (var i = 0; i < password.length; i++) {
                 code = password.charCodeAt(i);
                 
                 for (var j = i+1; i > 0; i--) {
                     hash *= code;
                 }
+            }*/
+
+            for (var i = 0; i < password.length; i++) {
+                code = password.charCodeAt(i);
+                hash += code;
             }
 
             return hash;
