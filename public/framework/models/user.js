a9ab01a3bc2aa05c451fc9c503f0ab36b00f62cc
pfacheris <plfacheris@gmail.com>
2013-02-05 01:31:19 -0500

    added front-end user model

--- public/framework/models/user.js
+++ public/framework/models/user.js
@@ -1,7 +1,35 @@
 window.User = Backbone.Model.extend({
-    defaults: {
-        isLoggedIn: false
+    urlRoot: "/users",
+    idAttribute: "_id",
+
+    validate: function (attrs) {
+        var invalid = [];
+        
+        if(![a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.test(attrs.email)) invalid.push("email");
+        if (attrs.firstName.length <= 0) invalid.push("first name");
+        if (attrs.lastName.length <= 0) invalid.push("last name");
+        if (attrs.password.length < 6) invalid.push("password length");
+
     },
+
+    defaults: {
+        _id: null,
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        isLoggedIn: false,
+    },
+
+    logout: function() {
+        this.isLoggedIn = false;
+    }
+
+    login = function(userName, password)
+    {
+        
+        this.save();
+    }
 
 
 });
---
+++ public/framework/routes.js
@@ -0,0 +1,45 @@
+var AppRouter = Backbone.Router.extend({
+
+    routes: {
+        ""                  : "index",
+        "home"            	: "home",
+        "me"      	        : "settings",
+        "about"             : "about"
+    },
+
+    initialize: function () {
+        this.headerView = new HeaderView();
+        $('.header').html(this.headerView.el);
+    },
+
+    home: function (id) {
+        if (!this.homeView) {
+            this.homeView = new HomeView();
+        }
+        $('.content').html(this.homeView.el);
+        this.headerView.selectMenuItem('home-menu');
+    },
+
+    settings: function (id) {
+        if (!this.homeView) {
+            this.homeView = new SettingsView();
+        }
+        $('.content').html(this.homeView.el);
+        this.headerView.selectMenuItem('settings-menu');
+    },
+
+    about: function () {
+        if (!this.aboutView) {
+            this.aboutView = new AboutView();
+        }
+        $('.content').html(this.aboutView.el);
+        this.headerView.selectMenuItem('about-menu');
+    }
+
+});
+
+utils.loadTemplate(['IndexView', 'HomeView', 'HeaderView', 'AboutView'], function() {
+    app = new AppRouter();
+    Backbone.history.start();
+});
+
