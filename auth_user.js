a2f4b52d00049c6dd4271ec82ada1869944a537d
Chae Jubb <cjubb39@gmail.com>
2013-02-05 02:28:35 -0500

    Fixing method for calling hash function.  Fix0

--- auth_user.js
+++ auth_user.js
@@ -1,4 +1,4 @@
-utilities = require('utilities');
+utilities = require('./utilities');
 
 var COLLECTION_NAME = 'users';
 module.exports = function (db, BSON) {
@@ -35,24 +35,5 @@ module.exports = function (db, BSON) {
                 }
             });
         },
-}
-        pwHash: function(password) {
-            var hash = 0;
-            var code;
-            
-            /*for (var i = 0; i < password.length; i++) {
-                code = password.charCodeAt(i);
-                
-                for (var j = i+1; i > 0; i--) {
-                    hash *= code;
-                }
-            }*/
-
-            for (var i = 0; i < password.length; i++) {
-                code = password.charCodeAt(i);
-                hash += code;
-            }
-
-            return hash;
-        }
     }
+}
