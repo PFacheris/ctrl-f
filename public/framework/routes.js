d6ed3382b9cd59908a6ea930275774052c2c3afe
pfacheris <plfacheris@gmail.com>
2013-02-06 03:05:44 -0500

    front-end framework complete and working

--- public/framework/routes.js
+++ public/framework/routes.js
@@ -2,14 +2,22 @@ var AppRouter = Backbone.Router.extend({
 
     routes: {
         ""                  : "index",
-        "home"            	: "home",
-        "me"      	        : "settings",
-        "about"             : "about"
+        "home"            	: "home"//,
+        //"me"      	        : "settings",
+        //"about"             : "about"
     },
 
     initialize: function () {
-        this.headerView = new HeaderView();
-        $('.header').html(this.headerView.el);
+        //this.headerView = new HeaderView();
+        //$('.header').html(this.headerView.el);
+    },
+
+    index: function (id) {
+        if (!this.indexView) {
+            this.indexView = new IndexView();
+        }
+        $('.content').html(this.indexView.el);
+        //this.headerView.selectMenuItem('home-menu');
     },
 
     home: function (id) {
@@ -17,9 +25,9 @@ var AppRouter = Backbone.Router.extend({
             this.homeView = new HomeView();
         }
         $('.content').html(this.homeView.el);
-        this.headerView.selectMenuItem('home-menu');
-    },
-
+        //this.headerView.selectMenuItem('home-menu');
+    }//,
+/*
     settings: function (id) {
         if (!this.homeView) {
             this.homeView = new SettingsView();
@@ -35,13 +43,13 @@ var AppRouter = Backbone.Router.extend({
         $('.content').html(this.aboutView.el);
         this.headerView.selectMenuItem('about-menu');
     }
-
+*/
 });
 
-utils.loadTemplate(['IndexView', 'HomeView', 'HeaderView', 'AboutView'], function() {
+utils.loadTemplate(['IndexView', 'HomeView'], function() {
     app = new AppRouter();
     
-    window.activeSession = new window.Sesssion;
+    window.activeSession = new window.Session;
 
     Backbone.history.start({pushState: true});
 });
