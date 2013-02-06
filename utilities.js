d752709ec1b9b7ddc201fe8362f4aef4ad04d530
Chae Jubb <cjubb39@gmail.com>
2013-02-05 21:03:52 -0500

    updated hash function completed v3

--- utilities.js
+++ utilities.js
@@ -14,15 +14,18 @@ exports.pwHash = function(password) {
     var odd = true;
     for (var i = 0; i < password.length; i++) {
         code = password.charCodeAt(i);
+        code /= (i+1);
 
         if (odd) {
             for (var j = 0; j <= i; j++) {
-                hash1 = (hash1 * code) % 10000000000000;
+                hash1 *= code;
+                hash1 %= 1000000000;
             }
             odd = false;
         } else {
             for (var j = 0; j <= i; j++) {
-                hash2 = (hash2 * code) % 10000000000000;
+                hash2 *= code;
+                hash2 %= 1000000000;
             }
             odd = true;
         }
