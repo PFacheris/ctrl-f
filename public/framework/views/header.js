3bb519eb0f75922b64d824d83c15c1dfb9fbbfc6
pfacheris <plfacheris@gmail.com>
2013-02-07 02:13:31 -0500

    transfer events to backbone

--- public/framework/views/header.js
+++ public/framework/views/header.js
@@ -1,12 +1,38 @@
 window.HeaderView = Backbone.View.extend({
 
-    initialize:function () {
+    initialize: function () {
         this.render();
     },
 
-    render:function () {
+    render: function () {
+        var self = this;
         $(this.el).html(this.template());
+        $(this.el).find('nav').css('background', 'none');
+        $(this.el).find('#header').animate(
+            {top: '0px'},
+            500,
+            function() {
+                $('.ribbon').animate({marginTop: '-28px'}, 500);
+            }
+        );
         return this;
-    }
+    },
+    
+    events: {
+        "click #btnSettings"    : "toggleLoginBox"
+    },
 
+    toggleLoginBox: function () {
+        var newOpacity = ($('#login').css('opacity') == 0) ? 1 : 0;
+        $('#login').animate({opacity: newOpacity}, 500);
+    }/*,
+
+    showActive: function (menuItem) {
+        $('.ribbon').removeClass('active');
+        if (menuItem) {
+            $('.' + menuItem).addClass('active');
+        }
+    }
+*/
 });
+
