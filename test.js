92001cac8b76f38422645270968109e12cf11786
cjubb39 <cjubb39@gmail.com>
2013-02-09 04:41:51 -0500

    testing functionalitiy and syntax of setInterval() function on updateParcelStati v1.001

---
+++ test.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+    return {
+        testFunction : function (request,response) {
+  //          setInterval(function(err, result){
+console.log('A message');
+//}, 5000);
+        }
+    }
+}
