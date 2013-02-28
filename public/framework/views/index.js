6748549a7f0bfca9f3c415f865d889f35e7b4403
pfacheris <plfacheris@gmail.com>
2013-02-28 12:33:34 -0500

    index page tracking fix

--- public/framework/views/index.js
+++ public/framework/views/index.js
@@ -22,12 +22,14 @@ window.IndexView = Backbone.View.extend({
         var $fedex1 = /(\b96\d{20}\b)|(\b\d{15}\b)|(\b\d{12}\b)/;
         var $fedex2 = /\b((98\d\d\d\d\d?\d\d\d\d|98\d\d) ?\d\d\d\d ?\d\d\d\d( ?\d\d\d)?)\b/;
         var $fedex3 = /^[0-9]{15}$/;
+        var $fedex4 = /^[0-9]{20}$/;
         var $usps1 = /\b(91\d\d ?\d\d\d\d ?\d\d\d\d ?\d\d\d\d ?\d\d\d\d ?\d\d|91\d\d ?\d\d\d\d ?\d\d\d\d ?\d\d\d\d ?\d\d\d\d)\b/i;
         var $usps2 = /^E\D{1}\d{9}\D{2}$|^9\d{15,21}$/;
         var $usps3 = /^91[0-9]+$/;
         var $usps4 = /^[A-Za-z]{2}[0-9]+US$/;
+        var $usps5 = /^[0-9]{26}$/;
 
-        if ($usps1.test(trackingNumber) || $usps2.test(trackingNumber) || $usps3.test(trackingNumber) || $usps4.test(trackingNumber))
+        if ($usps1.test(trackingNumber) || $usps2.test(trackingNumber) || $usps3.test(trackingNumber) || $usps4.test(trackingNumber) || $usps5.test(trackingNumber))
         {
             package.set('service', 'usps');
         }
@@ -35,29 +37,42 @@ window.IndexView = Backbone.View.extend({
         {
             package.set('service', 'ups');
         }
-        else if ($fedex1.test(trackingNumber) || $fedex2.test(trackingNumber) || $fedex3.test(trackingNumber))
+        else if ($fedex1.test(trackingNumber) || $fedex2.test(trackingNumber) || $fedex3.test(trackingNumber) || $fedex4.test(trackingNumber))
         {
             package.set('service', 'fedex');
         }
-
+        
         if (package.get('service'))
         {
-            package.set('tracking', trackingNumber); 
-            package.save(null, {
-                success: function(model, result, xhr) {
+            deleteOverlays();
+            package.set('tracking', trackingNumber);
+            package.fetch({
+                data: {"tracking": package.get('tracking'), "service": package.get('service')},
+                success: function(model, result) {
                     utils.showAlert("Success!", "You tracked a package.");
                     setLocations(model.get('trackingInfo'));
-                    model.destroy();
                 },
-                error: function(model, xhr, options) {
-                    if (xhr.status == 400 || xhr.status == 417) {
-                        utils.showAlert("Warning", "We couldn't find any results for that tracking number, sorry.");
-                    }
-                    else {
-                        utils.showAlert("Error", "Something went wrong, we'll check it out.");
+                error: function(model, xhr) {
+                    if (xhr.status == 404 || xhr.status == 400 || xhr.status == 417)
+                    {
+                        package.save(null, {
+                            success: function(mod, result) {
+                                utils.showAlert("Success!", "You tracked a package.");
+                                setLocations(mod.get('trackingInfo'));
+                            },
+                            error: function(mod, xhr, options) {
+                                if (xhr.status == 400 || xhr.status == 417) {
+                                    utils.showAlert("Warning", "We couldn't find any results for that tracking number, sorry.");
+                                }
+                                else {
+                                    utils.showAlert("Error", "Something went wrong, we'll check it out.");
+                                }
+                            }
+                        });
                     }
                 }
             });
+
         }
         else
         {
